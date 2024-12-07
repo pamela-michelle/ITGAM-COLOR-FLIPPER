27df66346d241b72631f2769aa935a06f7ae8691
pamela-michelle <michi.sotelo090703@hotmail.com>
2024-12-07 06:55:25 +0000

    colores 💕☺️

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+btn.addEventListener("click", () => {
+    // Generando un número aleatorio entre 0 y el tamaño de colors
+    const randomNumber = getRandomNumber();
+    // Obteniendo el color de la lista de colores
+    const colorPicked = colors[randomNumber];
+    // Cambiando el color de fondo del body
+    document.body.style.backgroundColor = colorPicked;
+    // Cambiando el texto del span
+    color.textContent = colorPicked;
+  });
+  function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+  }
